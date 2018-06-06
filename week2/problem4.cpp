fbdfcfec7b1172139fd40a97a97271ba4c44f8a5
gaseln <eln.gasanov@gmail.com>
2018-06-07 00:53:13 +0300

    Create problem4.cpp

---
+++ week2/problem4.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+
+void UpdateIfGreater(const int& first, int& second) {
+    if (first > second) {
+        second = first;
+    }
+}
+/* int main() {
+    int a = 89;
+    int b = 77;
+    UpdateIfGreater(a, b);
+    std::cout << b << std::endl;
+} */
